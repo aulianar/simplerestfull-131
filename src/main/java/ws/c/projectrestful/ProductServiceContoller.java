41575cb17009f803fc313dd3027daa1c0add6999
aulianar <arisky540@gmail.com>
2022-12-04 20:36:37 +0700

    mengedit isi java controller product

--- src/main/java/ws/c/projectrestful/ProductServiceContoller.java
+++ src/main/java/ws/c/projectrestful/ProductServiceContoller.java
@@ -10,5 +10,22 @@ package ws.c.projectrestful;
  * @author USER DJOGJA
  */
 public class ProductServiceContoller {
-    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String id;
+    private String name;
 }
