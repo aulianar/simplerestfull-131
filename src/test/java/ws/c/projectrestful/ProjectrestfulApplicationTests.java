1d3caef60cae58dd76f8adc8a553966ab6690cc8
aulianar <arisky540@gmail.com>
2022-12-04 20:23:15 +0700

    create java productservicecontroller

---
+++ src/test/java/ws/c/projectrestful/ProjectrestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.c.projectrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
