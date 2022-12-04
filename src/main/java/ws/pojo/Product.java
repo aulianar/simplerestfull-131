394d1a2a20bce008f2b3617b0d1f792a263d12c4
aulianar <arisky540@gmail.com>
2022-12-04 20:38:12 +0700

    mengedit isi java controller product

--- src/main/java/ws/pojo/Product.java
+++ src/main/java/ws/pojo/Product.java
@@ -10,5 +10,23 @@ package ws.pojo;
  * @author USER DJOGJA
  */
 public class Product {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    private String id;
+    private String name;
 }
